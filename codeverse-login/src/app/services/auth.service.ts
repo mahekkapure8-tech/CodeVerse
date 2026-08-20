162757cf6993f4e360e32e572e5e0adc2ee36224
Mahek <mahekkapure8@gmail.com>
2026-08-20 18:30:18 +0530

    Update API URL for production

--- codeverse-login/src/app/services/auth.service.ts
+++ codeverse-login/src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   private apiUrl =
-    'http://localhost:5000/api/auth';
+    'https://codeverse-api-g7b5.onrender.com/api/auth';
 
   constructor(private http: HttpClient) {}
 
